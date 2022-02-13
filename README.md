4fc828eba91cd7ac16fdd8fb70d24defde178e82
Felix Haus <felixhaus@gmail.com>
2022-02-13 15:45:49 +0100

    v0.1.0

---
+++ README.md
@@ -0,0 +1,16 @@
+# Development container homebrew utils
+
+# Features In This Repo
+
+### homebrew-install
+
+Install homebrew packages.
+
+```jsonc
+"features": {
+    "milliHQ/dev-container-features-homebrew-utils/homebrew-install": {
+        "tap": "aws/tap",
+        "packages": "aws-sam-cli"
+    },
+}
+```
---
+++ install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+set -e
+
+# The install.sh script is the installation entrypoint for any dev container 'features' in this repository.
+
+# Verify we're on a supported OS
+. /etc/os-release
+if [ "${ID}" != "debian" ] &&  [ "${ID_LIKE}" != "debian" ]; then
+cat << EOF
+*********** Unsupported operating system "${ID}" detected ***********
+Features support currently requires a Debian/Ubuntu-based image. Update your
+image or Dockerfile FROM statement to start with a supported OS. For example:
+mcr.microsoft.com/vscode/devcontainers/base:ubuntu
+Aborting build...
+EOF
+    exit 2
+fi
+
+# The tooling will parse the devcontainer-features.json + user devcontainer, and write
+# any build-time arguments into a feature-set scoped "devcontainer-features.env"
+# The author is free to source that file and use it however they would like.
+set -a
+. ./devcontainer-features.env
+set +a
+
+
+if [ ! -z ${_BUILD_ARG_HOMEBREW_INSTALL} ]; then
+    echo "Activating feature 'homebrew-install'"
+
+    # Check if brew command is available
+    if [ ! command -v brew &> /dev/null ]; then
+        cat << EOF
+*********** homebrew not installed ***********
+The command `brew` could not be found on the system.
+Please make sure to add the homebrew feature before this feature.
+Aborting build...
+EOF
+        exit 2
+    fi
+
+    # Taps
+    BREW_TAPS=${_BUILD_ARG_HOMEBREW_INSTALL_TAP}
+
+    if [ "$BREW_TAPS" != "" ]; then
+        echo "Tapping the following repositories: ${BREW_TAPS}"
+        BREW_TAPS_ARRAY=( $BREW_TAPS )
+
+        for i in ${!BREW_TAPS_ARRAY[@]}
+        do
+            brew tap "${BREW_TAPS_ARRAY[i]}"
+        done
+    fi
+
+    # Install
+    BREW_PACKAGES=${_BUILD_ARG_HOMEBREW_INSTALL_PACKAGES}
+    if [ "$BREW_PACKAGES" != "" ]; then
+        echo "Installing brew packages: ${BREW_PACKAGES}"
+        brew install "${BREW_PACKAGES}"
+    fi
+fi
